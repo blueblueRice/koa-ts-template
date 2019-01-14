9c5fd1e6271f749e5ea927b01d61606b2ddf53d7
bluebluerice <18316961025@163.com>
2019-01-14 16:23:01 +0800

    初始化项目

--- dist/service/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Service {
-    sayHello(ctx) {
-        ctx.body = 'hello';
-    }
-}
-exports.default = Service;
-//# sourceMappingURL=index.js.map
